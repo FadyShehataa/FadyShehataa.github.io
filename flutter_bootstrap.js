b60bf58785028aff0007810d4ef1981dfc0bd17c
FadyShehataa <fadyshehata40@gmail.com>
2024-12-17 20:25:54 +0200

    add 2 buttons to scroll

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2017059659"
+    serviceWorkerVersion: "1675990082"
   }
 });
